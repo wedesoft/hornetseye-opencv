9c1bd08b67cd0e56c2dd5eb0b99641ad54cf00ee
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-06-30 12:33:58 +0100

    Update version number

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 require 'rake'
 
 PKG_NAME = 'hornetseye-opencv'
-PKG_VERSION = '0.3.5'
+PKG_VERSION = '0.3.6'
 CFG = RbConfig::CONFIG
 CXX = ENV[ 'CXX' ] || 'g++'
 RB_FILES = ['config.rb'] + FileList[ 'lib/**/*.rb' ]
